8130e48ef0d588548bfb2b6a6edf5480d961e09f
MilicaKaretic <milicakaretic1@gmail.com>
2020-06-29 19:07:03 +0200

    refactor code and add comments

--- DAN_XLI_Milica_Karetic/MainWindow.xaml.cs
+++ DAN_XLI_Milica_Karetic/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace DAN_XLI_Milica_Karetic
 {
@@ -23,10 +13,22 @@ namespace DAN_XLI_Milica_Karetic
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// backgroundworker instance
+        /// </summary>
         static BackgroundWorker backgroundWorker;
+        /// <summary>
+        /// text to print
+        /// </summary>
         static string text;
+        /// <summary>
+        /// number of copies
+        /// </summary>
         static string copyNum;
 
+        /// <summary>
+        /// MainWindow constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -40,6 +42,11 @@ namespace DAN_XLI_Milica_Karetic
             backgroundWorker.ProgressChanged += worker_ProgressChanged;
         }
         
+        /// <summary>
+        /// Method for printing text to file
+        /// </summary>
+        /// <param name="fileName">File location</param>
+        /// <param name="text">Text to print</param>
         private void Print(string fileName, string text)
         {
             string file = fileName + ".txt";
@@ -51,37 +58,58 @@ namespace DAN_XLI_Milica_Karetic
             }
         }
 
+        /// <summary>
+        /// Do work method implementation
+        /// </summary>
+        /// <remarks>
+        /// Print typed text into files
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void worker_DoWork(object sender, DoWorkEventArgs e)
         {
+            //calculate percentage
             int c = 100 / int.Parse(copyNum);
 
+            //for loop for printing text to specified number of files(number of copies)
             for (int i = 1; i <= int.Parse(copyNum); i++)
             {
                 Thread.Sleep(1000);
                 string fileName = (i).ToString() + "." + DateTime.Now.ToString("dd_MM_yyyy_hh_mm");
                 
+                //if cancel print button clicked
                 if(backgroundWorker.CancellationPending)
                 {
                     e.Cancel = true;
                     backgroundWorker.ReportProgress(0);
                     return;
                 }
+                //call print method for printing text to files
                 Print(fileName, text);
                 backgroundWorker.ReportProgress(i * c);
             }
+            //completed message
             e.Result = "Completed";
         }
 
+        /// <summary>
+        /// Method for displaying result message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            //if cancel button is clicked
             if (e.Cancelled)
             {
                 lblMessage.Content = "Processing cancelled";
             }
+            //if errro occured
             else if (e.Error != null)
             {
                 lblMessage.Content = e.Error.Message;
             }
+            //if printing is done
             else
             {
                 lblMessage.Content = e.Result.ToString();
@@ -90,12 +118,22 @@ namespace DAN_XLI_Milica_Karetic
                     
         }
 
+        /// <summary>
+        /// Method for displaying progress bar work
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             progressBar.Value = e.ProgressPercentage;
             lblMessage.Content = e.ProgressPercentage.ToString() + " %";
         }
 
+        /// <summary>
+        /// Button cancel print click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CancelPrint_Click(object sender, RoutedEventArgs e)
         {
             if (backgroundWorker.IsBusy)
@@ -108,6 +146,11 @@ namespace DAN_XLI_Milica_Karetic
             }
         }
 
+        /// <summary>
+        /// Button print click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Print_Click(object sender, RoutedEventArgs e)
         {
             if(!backgroundWorker.IsBusy)
@@ -122,6 +165,11 @@ namespace DAN_XLI_Milica_Karetic
            
         }
 
+        /// <summary>
+        /// Method for text change event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TxtText_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox objTextBox = (TextBox)sender;
@@ -129,11 +177,15 @@ namespace DAN_XLI_Milica_Karetic
             if (text != null && copyNum != null)
             {
                 btnPrint.IsEnabled = true;
-                //btnCancel.IsEnabled = true;
             }
                 
         }
 
+        /// <summary>
+        /// Method for number of copies changed event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TxtCopyNum_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox objTextBox = (TextBox)sender;
@@ -149,8 +201,6 @@ namespace DAN_XLI_Milica_Karetic
                 {
                     MessageBox.Show("Please enter number");
                 }
-                
-                //btnCancel.IsEnabled = true;
             }
                 
         }
